30452f94512cec3359c14d340eb4a35191d1a2b3
TiM3er <33333118+TiM3er@users.noreply.github.com>
2017-11-05 16:24:11 +0100

    complete

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,36 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+	public static void wypisztablice(int[] arr)
+	{
+		for (int i = 0 ;i<arr.length;i++) {
+			System.out.print(arr[i] + " ");
+		} 
+	}
+	public static void wypisztablice(int[] arr,int x ,int y)
+	{
+		for (int i = 0 ;i<arr.length;i++) {
+			if (i==x)
+			{
+				System.out.print("[");
+				System.out.print(arr[i]);
+
+				System.out.print("] ");
+			}
+			else if (i==y)
+			{
+				System.out.print("[");
+				System.out.print(arr[i]);
+
+				System.out.print("] ");
+			}
+			else
+			{
+				System.out.print(arr[i] + " ");
+
+			}
+		} 
+	}
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
@@ -14,26 +44,35 @@ public class Main {
 		for (int i = 0 ;i<n;i++) {
 			arr[i]=rand.nextInt(100);
 		}
-		for (int i = 0 ;i<n;i++) {
-			System.out.print(arr[i]);
-		} 
+		wypisztablice(arr);
+		System.out.println("\n\n\n\n");
 		
 		
 		
-		
-		
+		int licznik = 0;
         int temp = 0;  
-         for(int i=0; i < n; i++){  
-                 for(int j=1; j < (n-i); j++){  
-                          if(arr[j-1] > arr[j]){  
-                                 //swap elements  
-                                 temp = arr[j-1];  
-                                 arr[j-1] = arr[j];  
-                                 arr[j] = temp;  
-                         }  
-                          
-                 }  
-         }  
+        for(int i=0; i < n; i++){  
+        	for(int j=1; j < (n-i); j++){  
+        		licznik++;
+        		if(arr[j-1] > arr[j]){  
+        			System.out.println(arr[j-1]+ " > " +arr[j]);
+        			temp = arr[j-1];  
+        			arr[j-1] = arr[j];  
+        			arr[j] = temp;  
+        		}  
+        		else if (arr[j-1]<arr[j])
+        		{
+        			System.out.println(arr[j-1]+ " < " +arr[j]);
+        		}
+        		else if (arr[j-1]==arr[j])
+        		{
+        			System.out.println(arr[j-1]+ " = " +arr[j]);
+        			
+        		}
+        		wypisztablice(arr,j-1,j);
+        		System.out.println("\nPodejscie: " + licznik);
+        	}  
+     	}  
 	}
 
 }
